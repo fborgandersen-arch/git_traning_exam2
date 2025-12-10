f729798eae4e7fa5356d8f94995b8531a40ae9b4
[fborgandersen-arch] <[fborgandersen@gmail.com]>
2025-12-10 19:36:05 +0000

    FIX

--- test_packages.R
+++ test_packages.R
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-library(tidyverse) # CAN WE Conflict???
-=======
-library(ggplot2) #lets see if we can create a conflict
->>>>>>> b2adc8fc6d15f68abd2bbcf0b621fd7cd40db58d
+library(ggplot2) # Fixed conflict
